f64454928fdeaa092b9a6f30c1570728d052260f
BrunoJGiacomini <brunojunigiacomini@gmail.com>
2024-11-12 10:00:12 -0300

    Services Delete and Update

--- src/main/java/br/fatec/product/controllers/ProductController.java
+++ src/main/java/br/fatec/product/controllers/ProductController.java
@@ -2,9 +2,15 @@ package br.fatec.product.controllers;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +25,30 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(service.getAllProducts());
     }
     @GetMapping("{id}")
-    public Product geProduct(@PathVariable long id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> geProduct(@PathVariable long id){
+        return ResponseEntity.ok(service.getProductById(id));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable long id, @RequestBody Product product){
+        service.update(product, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{id}")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+        Product newProduct = service.save(product);
+        return ResponseEntity.created(null).body(newProduct);
+    }
+
 }
